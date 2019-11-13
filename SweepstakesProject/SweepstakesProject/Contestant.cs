1b7997d84484fe8f9624152501ebd7fa6501b9c9
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-11-13 14:31:21 -0600

    Made my contestant class, trying to figure out what to do next

---
+++ SweepstakesProject/SweepstakesProject/Contestant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public int registrationNumber;
+        public Contestant()
+        {
+            
+        }
+
+        public void FirstName()
+        {
+            Console.WriteLine("Please enter your first name.");
+            firstName = Console.ReadLine();
+        }
+
+        public void LastName()
+        {
+            Console.WriteLine("Please enter your Last name.");
+            lastName = Console.ReadLine();
+        }
+
+        public void Email()
+        {
+            Console.WriteLine("");
+            email = Console.ReadLine();
+        }
+
+        public void RegistrationNumber()
+        {
+            registrationNumber = 10;
+        }
+    }
+}
